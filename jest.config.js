6c483cbf27e764fd063a5927f0e7a2654f6a2050
teerryn <slaughterer90@gmail.com>
2019-10-17 00:12:31 +0100

    Fix eslint warnings

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  preset: 'jest-expo',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  setupFiles: ['./jestSetupFile.js'],
-  testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
-  moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
-  globals: {
-    'ts-jest': {
-      tsConfig: {
-        jsx: 'react',
-      },
-      diagnostics: false,
+    preset: 'jest-expo',
+    transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.tsx?$': 'ts-jest',
     },
-  },
+    setupFiles: ['./jestSetupFile.js'],
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
+    moduleFileExtensions: ['js', 'ts', 'tsx', 'json'],
+    globals: {
+        'ts-jest': {
+            tsConfig: {
+                jsx: 'react',
+            },
+            diagnostics: false,
+        },
+    },
 };
