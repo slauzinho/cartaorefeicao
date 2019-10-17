043d529891455da52f6b73c8efff28408dd60c55
teerryn <slaughterer90@gmail.com>
2019-10-17 08:23:56 +0100

    Fix eslint warnings for NoCardScreen

--- screens/NoCardScreen.tsx
+++ screens/NoCardScreen.tsx
@@ -1,30 +1,52 @@
 import React, { FunctionComponent } from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Button } from 'react-native-elements';
 import { material } from 'react-native-typography';
 import { NavigationInjectedProps } from 'react-navigation';
 
 const NoCardScreen: FunctionComponent<NavigationInjectedProps> = props => (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Image source={require('../assets/images/nocc.png')} style={{ width: 363, height: 256 }} />
-        <View style={{ marginTop: 20 }}>
-            <Text style={[material.title, { color: 'rgba(0,0,0,0.4)' }]}>{'Não tem nenhum cartão guardado.'}</Text>
-        </View>
-        <View style={{ marginTop: 50 }}>
+    <View style={styles.container}>
+        <Image source={require('../assets/images/nocc.png')} style={styles.image} />
+        <Text style={[material.title, styles.infoMessage]}>{'Não tem nenhum cartão guardado.'}</Text>
+        <View style={styles.buttonContainer}>
             <Button
                 title="ADICIONAR"
-                titleStyle={{ fontWeight: '700', color: 'black' }}
-                buttonStyle={{
-                    backgroundColor: 'rgba(255, 255, 255, 1)',
-                    width: 300,
-                    height: 45,
-                }}
+                titleStyle={styles.placeholder}
+                buttonStyle={styles.button}
                 onPress={() => props.navigation.navigate('Add')}
                 icon={<Icon name="plus" size={15} />}
             />
         </View>
     </View>
 );
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 363,
+        height: 256,
+    },
+    infoMessage: {
+        marginTop: 20,
+        color: 'rgba(0, 0, 0, 0.4)',
+    },
+    buttonContainer: {
+        marginTop: 50,
+    },
+    button: {
+        backgroundColor: 'rgba(255, 255, 255, 1)',
+        width: 300,
+        height: 45,
+    },
+    placeholder: {
+        fontWeight: '700',
+        color: 'black',
+    },
+});
 
 export default NoCardScreen;
